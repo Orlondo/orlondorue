2a08c4e39d789acc2716f5b8a7fe41fe661d3879
Orlondo <o.rue@sbcglobal.net>
2020-02-16 23:14:37 -0500

    Added InnOcean Hankook 2/20

---
+++ InnOcean/hankook-mlb-022020/300x250/scripts.js
@@ -0,0 +1,33 @@
+var io = {};
+
+io.animations = (function() {
+  "use strict";
+
+  const container = document.querySelector('.container');
+  var slash = document.querySelector('.slash');
+  const image = document.querySelector('.background');
+  const tire = document.querySelector('.tire');
+  const gradient = document.querySelector('.gradient');
+  const lockup = document.querySelector('.lockup');
+
+  onStart();
+
+  function onStart() {
+    setTimeout(function () { slash.classList.add('revealSlash') }, 500);
+    frame1();
+  }
+
+  function frame1() {
+    setTimeout(function () { 
+      image.classList.add('revealBackgroundImage');
+      gradient.classList.add('reveal')
+      tire.classList.add('revealTire');
+      container.classList.add('revealContent');
+    }, 3000);
+    endFrame();
+  }
+
+  function endFrame() {
+    setTimeout(function () { lockup.classList.add('reveal') }, 6000);
+  }
+})();
