541dce4b8091363717dfb5815669d90065485d77
Orlondo <o.rue@sbcglobal.net>
2020-08-28 01:23:57 -0400

    Added Brand 728x90

---
+++ InnOcean/pacific-life/728x90-brand/main.js
@@ -0,0 +1,44 @@
+(function init() {
+  io = {};
+
+  var backup = document.querySelector('.backup');
+  var container = document.querySelector('.container');
+  var copy = document.querySelector('.copy');
+  var cta = document.querySelector('.cta');
+  var logo = document.querySelector('.logo');
+
+  var endFrameTime = 4000;
+
+  io.showCopy = function () {
+    copy.classList.add('fadeIn');
+  };
+
+  io.showCta = function () {
+    cta.classList.add('fadeIn');
+  };
+
+  io.showLogo = function () {
+    logo.classList.add('fadeIn');
+  };
+
+  io.checkBrowser = function () {
+    console.log('browser check');
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE ');
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+      container.classList.add('hide');
+      backup.classList.remove('hide');
+    }
+
+    if (navigator.appVersion.indexOf('Edge') > -1) {
+      container.classList.add('hide');
+      backup.classList.remove('hide');
+    }
+  };
+
+  io.checkBrowser();
+  setTimeout(io.showCopy, 3000);
+  setTimeout(io.showLogo, endFrameTime);
+  setTimeout(io.showCta, endFrameTime);
+})();
